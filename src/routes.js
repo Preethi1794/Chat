827b1178bf7d11c64b7460e8ae0777308a7803b0
Preethi <preethimurthy1794@gmail.com>
2020-11-02 02:36:39 +0530

    Updates

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-export const SIGNIN = '/';
-export const CONTACT = '/contact';
-export const FOLDER = '/folder';
-export const COLLECTION = '/collection';
-export const CALENDAR = '/calendar';
-export const SETTINGS = '/settings';
-export const HOME = '/home';
